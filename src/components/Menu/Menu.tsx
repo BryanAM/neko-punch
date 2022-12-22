21bed220a1958ce4f6618b2eaec21ef85730049d
Bryan Aument <bryaument@gmail.com>
2022-12-22 12:39:13 -0500

    added gif toggle

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -14,15 +14,13 @@ function Menu({ open = false }: MenuProps) {
       <h2 className="menu-header">猫のフィルター</h2>
       <div className="menu-container">
         <div className="menu-item menu-toggle">
-          <p>GIFアニメーション</p>
+          <p>GIFアニメーションのみ</p>
           <Toggle change={() => { setGif(!gif)}} checked={gif} label="GIFアニメーション"/>
         </div>
 
-        <div className="menu-item menu-toggle">
-          <p>GIFアニメーション</p>
-          <Toggle change={() => { setGif(!gif)}} checked={gif} label="GIFアニメーション"/>
+        <div className="menu-item">
+    
         </div>
-        
       </div>
     </div>
   )
