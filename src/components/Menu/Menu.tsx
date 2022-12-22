63202f8dd70245f946b13cd24db36df7bd610b4f
Bryan Aument <bryaument@gmail.com>
2022-12-21 21:02:28 -0500

    added toggle for menu and menu component

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Toggle from '../Toggle/Toggle';
 import './menu.scss';
 
 
@@ -6,9 +7,12 @@ interface MenuProps {
   open: Boolean;
 }
 function Menu({ open = false }: MenuProps) {
+  const [gif, setGif] = useState(false);
+
   return (
     <div className={`menu ${open ? 'open' : ''}`}>
-      <h2>猫のフィルター</h2>
+      <h2 className="menu-header">猫のフィルター</h2>
+      <Toggle change={() => { setGif(!gif)}} checked={gif} label="GIFアニメーション"/>
     </div>
   )
 };
---
+++ src/components/Toggle/Toggle.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './toggle.scss';
+
+interface ToggleProps {
+  change: () => void;
+  checked: boolean | undefined;
+  label: string;
+};
+
+function Toggle({change, checked, label}: ToggleProps){
+
+  return (
+    <label className="toggle">
+      {label}
+      <input type="checkbox" onChange={change} checked={checked}/>
+      <span className="slider round"></span>
+    </label>
+  )
+};
+
+export default Toggle;
